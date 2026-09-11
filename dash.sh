21c5e23b9e3a7b36a868688938dd73f9510ee61b
C1sc066 <146997353+C1sc066@users.noreply.github.com>
2026-09-11 13:52:45 -0300

    Teste cores

--- dash.sh
+++ dash.sh
@@ -7,7 +7,9 @@
 # ==============================================================================
 
 # Cores e Estilos ANSI (Degradê Suave de 6 Estágios)
-if [ -t 1 ]; then
+# Cores e Estilos ANSI (Compatível com curl ... | bash)
+# Se stdout for terminal OU se $TERM estiver definido e não for "dumb", ativa cores
+if [ -t 1 ] || [ -n "$TERM" ] && [ "$TERM" != "dumb" ]; then
     CLR_RESET="\033[0m"
     CLR_BOLD="\033[1m"
     CLR_DIM="\033[2m"
